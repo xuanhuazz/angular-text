49ffd59b7f684a24152e7b0b370a63f104b76984
Leo Li (李可化) <likehua@inspur.com>
2022-01-21 11:20:24 +0800

    finish route

--- Myangular/src/app/home-detail/home-detail.component.ts
+++ Myangular/src/app/home-detail/home-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit,Input } from '@angular/core';
 
 //引入类型文件
 import {Homesx} from '../homesx/homesx' 
+//引入获取路由参数的依赖
+import { ActivatedRoute } from '@angular/router';//保存路由信息，提取参数用的
+import { Location } from '@angular/common';      //一个angular的服务
+import { HomeService } from '../home.service';  //获取数据
 @Component({
   selector: 'app-home-detail',
   templateUrl: './home-detail.component.html',
@@ -10,10 +14,15 @@ import {Homesx} from '../homesx/homesx'
 
 export class HomeDetailComponent implements OnInit {
 
-  @Input() home?: Homesx;   //接收数据需要用@Input()
-  constructor() { }
-
+  // @Input() home?: Homesx;   //接收数据需要用@Input()
+  home:Homesx | undefined //定义home数组
+  constructor(private acroute:ActivatedRoute,private location:Location,private homeService:HomeService) { }
+  getDetail():void {
+    const id = Number(this.acroute.snapshot.paramMap.get('id')) //得到参数的语法
+    this.homeService.getId(id).subscribe(h => this.home = h)  //通过getId方法，得到匹配后的信息
+  }
   ngOnInit(): void {
+    this.getDetail()
   }
 
 }
--- Myangular/src/app/home.service.ts
+++ Myangular/src/app/home.service.ts
@@ -10,10 +10,17 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HomeService {
+  //获取home姓名的方法
   getHome():Observable<Homesx[]> {
     const home = of(HOME)
     this.messageService.add('获取数据');//当获取到数据时，发送消息
     return home
+  }
+  //获取参数id的方法
+  getId(id:number):Observable<Homesx|undefined> {
+    const home = HOME.find(h =>h.id === id)  //通过find
+    this.messageService.add(`得到id${{home}}`);//当获取到数据时，发送消息
+    return of(home)
   }
   constructor(private messageService:MessageService) { }
 }
